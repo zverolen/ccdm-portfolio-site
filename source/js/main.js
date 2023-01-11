3a8a662226c37d70c7605eea7a27fa7e0b47506c
Lena Zvereva <zverolen@gmail.com>
2023-01-11 07:36:11 +0100

    Adds changes for collapsible section

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,19 @@
 const toggleButtons = document.querySelectorAll('.js-button');
 
+const switchButtonLabel = (labels) => {
+  for (const label of labels) {
+    const isHidden = label.hidden;
+    label.hidden = !isHidden;
+  }
+}
+
 toggleButtons.forEach(button => {
   button.onclick = () => {
-    const target = button.previousElementSibling;
+    const target = button.parentElement.previousElementSibling.querySelector('.js-collapsible');
     const expanded = button.getAttribute('aria-expanded') === 'true';
     button.setAttribute('aria-expanded', !expanded);
     target.hidden = expanded;
+    
+    switchButtonLabel(button.children);
   }
 });
